b9b40c02ecc96a820e4b774b818c6e60cb08eb81
pidokige02 <pidokige0204@gmail.com>
2022-03-12 14:10:51 +0900

    commit until lesson4

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Dashboard from "../views/Dashboard.vue";
+import RegisterUser from "../views/RegisterUser.vue";
+import LoginUser from "../views/LoginUser.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,22 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterUser,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginUser,
   },
   {
     path: "/about",
@@ -25,6 +44,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem("user");
+
+  if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
+    next("/");
+  }
+  next();
 });
 
 export default router;
